5f312f84d0cf0fcde13ad33f9b3f2f564e5ce882
Shakil57375 <shakil57375@gmail.com>
2023-05-02 10:13:37 +0600

    ActiveLinks added on header

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,44 +1,54 @@
 /* eslint-disable no-unused-vars */
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
-import './Header.css'
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import "./Header.css";
+import ActiveLink from "../../ActiveLink/ActiveLink";
 const Header = () => {
-    const [open, setOpen] = useState(false);
-    return (
-        <div className='flex justify-between w-full  items-center lg:px-28 px-4 lg:py-6 py-4 bg-gray-700 '>
-            <div className='text-3xl font-bold cursor-pointer'>
-                <Link to= "/" className='sm:m-0 sm:p-0 text-white'>Chef Master</Link>    
-            </div>
-            <div className='flex justify-between items-center flex-row-reverse lg:flex-row'>
-                <div>
-                    <div className='md:hidden' onClick={()=> setOpen(!open)}>
-                            <span >
-                                {
-                                    open === true ? <XMarkIcon className='h-6  w-6 text-white'/> : <Bars3Icon className='h-6  w-6 text-white'/>
-                                    
-                                }
-                            </span>
-                    </div>
-                    <div className={`links flex text-white absolute lg:static duration-500 lg:flex-row flex-col justify-center  ${open ? 'block right-0 top-16 -ml-16 z-50 lg:bg-inherit bg-black lg:text-black text-white py-5 gap-2 pl-10' : "hidden lg:block"} `}>
-                  <Link to="/">Home</Link>
-                  <Link to="/blogs">Blogs</Link>
-                  <Link to="/login">Login</Link>
-                  <Link to="/register">Register</Link>
-                    </div>
-                </div>
-                <div>
-                {/* {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="flex justify-between w-full  items-center lg:px-28 px-4 lg:py-6 py-4 bg-gray-700 ">
+      <div className="text-3xl font-bold cursor-pointer">
+        <Link to="/" className="sm:m-0 sm:p-0 text-white">
+          Chef Master
+        </Link>
+      </div>
+      <div className="flex justify-between items-center flex-row-reverse lg:flex-row">
+        <div>
+          <div className="md:hidden" onClick={() => setOpen(!open)}>
+            <span>
+              {open === true ? (
+                <XMarkIcon className="h-6  w-6 text-white" />
+              ) : (
+                <Bars3Icon className="h-6  w-6 text-white" />
+              )}
+            </span>
+          </div>
+          <div
+            className={`links flex text-white absolute lg:static duration-500 lg:flex-row flex-col justify-center text-lg ${
+              open
+                ? "block right-0 top-16 -ml-16 z-20 lg:bg-inherit bg-black lg:text-black text-white py-5 gap-2 pl-10"
+                : "hidden lg:block"
+            } `}
+          >
+            <ActiveLink to="/">Home</ActiveLink>
+            <ActiveLink to="/blogs">Blogs</ActiveLink>
+            <ActiveLink to="/login">Login</ActiveLink>
+            <ActiveLink to="/register">Register</ActiveLink>
+          </div>
+        </div>
+        <div>
+          {/* {
               user && 
               <div className="flex gap-4">
                 <button className="text-white" onClick={handleLogOut}>Sign Out</button>
                 <span className='text-white mr-3'> <img src={user.photoURL} width={50} height={50} className="rounded-full" alt="" /></span>
               </div>
             } */}
-                </div> 
-            </div>          
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default Header;
