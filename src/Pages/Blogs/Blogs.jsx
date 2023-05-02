fe7aa73ca18f963113eb5ccd83417bafc251285c
Shakil57375 <shakil57375@gmail.com>
2023-05-02 09:56:05 +0600

    header added and layout added also

---
+++ src/Pages/Blogs/Blogs.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <p>Blogs</p>
+        </div>
+    );
+};
+
+export default Blogs;
